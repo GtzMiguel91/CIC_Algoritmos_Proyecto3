f689158c9804313e3fe49c7dac677017f03b1691
MGUTIE91 <eldisnak@gmail.com>
2022-05-18 23:45:12 -0500

    Proyecto3_algoritmo_Dijkstra
    Agregando proyecto 1 a main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import graphs_ex as p1
 import bfs_dfs_ex as p2
 import dijkstra_ex as p3
 
@@ -5,10 +6,13 @@ import dijkstra_ex as p3
 
 def main():
     try:
-        #Proyecto 2
+        #Proyecto 1 - Biblioteca de generación y manejo de grafos
+        #p1.graphs_examples()
+
+        #Proyecto_2 _Algoritmos_BFS_y_DFS
         #p2.bfs_dfs_examples()
         
-        #Proyecto 3
+        #Proyecto 3 - Algoritmo de Dijkstra
         p3.dijkstra_examples()
 
     except Exception as err:
