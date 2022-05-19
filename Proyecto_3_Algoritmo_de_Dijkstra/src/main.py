26c1449fd755abf11c4f00d92a9cdb0d891741c3
MGUTIE91 <eldisnak@gmail.com>
2022-05-18 23:21:06 -0500

    proyecto3_algoritmo_de_dijkstra
    Codigo fuente
    archivos gv
    imagenes

---
+++ Proyecto_3_Algoritmo_de_Dijkstra/src/main.py
@@ -0,0 +1,17 @@
+import bfs_dfs_ex as p2
+import dijkstra_ex as p3
+
+
+
+def main():
+    try:
+        #Proyecto 2
+        #p2.bfs_dfs_examples()
+        
+        #Proyecto 3
+        p3.dijkstra_examples()
+
+    except Exception as err:
+        print(err)
+
+main()
